f7a40f96d342e58c690b07e00895ee4380a226c9
Tyler Filla <tylerfilla@hotmail.com>
2019-03-31 12:08:29 -0500

    Get pretty good face correlation going

--- cozmonaut/facelib/facelib.cpp
+++ cozmonaut/facelib/facelib.cpp
@@ -24,14 +24,17 @@
 #include <pybind11/functional.h>
 #include <pybind11/pybind11.h>
 
-#include <spdyface.h>
-#include <spdyface_dlib.h>
+#include <dlib/matrix.h>
+#include <dlib/geometry/vector.h>
+#include <dlib/dnn.h>
+#include <dlib/image_transforms.h>
+#include <dlib/clustering.h>
 
 namespace py = pybind11;
 
 namespace facelib {
 
-/** An image frame. */
+/** An image in PIL raw format. */
 struct Image {
   /** The frame width. */
   int width;
@@ -43,18 +46,94 @@ struct Image {
   std::string bytes;
 };
 
+dlib::array2d<dlib::rgb_pixel> image_to_dlib_array(const Image& image) {
+  // Create a dlib array with the same size
+  // The height == # rows and the width == # cols
+  dlib::array2d<dlib::rgb_pixel> image_dlib(image.height, image.width);
+
+  // Quickly scan through the image to convert its bytes
+  // Python Image Library (PIL) uses a different format than dlib C++ library
+  for (int row = 0; row < image.height; ++row) {
+    for (int col = 0; col < image.width; ++col) {
+      // Compute pixel offset into the image
+      // This is the byte offset (into the PIL image) of the first pixel component
+      auto pixel_offset = 3ul * (row * image.width + col);
+
+      // Extract color components for this pixel
+      // This looks into the pixel a bit further for color data
+      auto r = (unsigned char) image.bytes[pixel_offset + 0];
+      auto g = (unsigned char) image.bytes[pixel_offset + 1];
+      auto b = (unsigned char) image.bytes[pixel_offset + 2];
+
+      // Store away the pixel in the dlib array
+      // This adopts dlib C++ format (with a custom pixel class)
+      image_dlib[row][col] = dlib::rgb_pixel(r, g, b);
+    }
+  }
+
+  return image_dlib;
+}
+
 /** A registry of faces. */
 class Registry {
   friend class Recognizer;
 
-  /** The map backing store. */
-  std::map<int, Image> m_store;
+  //
+  // The following types are from:
+  // https://github.com/davisking/dlib/blob/master/tools/python/src/face_recognition.cpp
+  //
 
-  /** The face detector. */
+  template<template<int, template<class> class, int, class> class block, int N, template<class> class BN,
+      typename SUBNET>
+  using residual = dlib::add_prev1<block<N, BN, 1, dlib::tag1<SUBNET>>>;
+
+  template<template<int, template<class> class, int, class> class block, int N, template<class> class BN,
+      typename SUBNET>
+  using residual_down = dlib::add_prev2<dlib::avg_pool<2, 2, 2, 2, dlib::skip1<dlib::tag2<block<N, BN, 2,
+      dlib::tag1<SUBNET>>>>>>;
+
+  template<int N, template<class> class BN, int stride, typename SUBNET>
+  using block = BN<dlib::con<N, 3, 3, 1, 1, dlib::relu<BN<dlib::con<N, 3, 3, stride, stride, SUBNET>>>>>;
+
+  template<int N, typename SUBNET>
+  using ares = dlib::relu<residual<block, N, dlib::affine, SUBNET>>;
+
+  template<int N, typename SUBNET>
+  using ares_down = dlib::relu<residual_down<block, N, dlib::affine, SUBNET>>;
+
+  template<class SUBNET>
+  using alevel0 = ares_down<256, SUBNET>;
+
+  template<class SUBNET>
+  using alevel1 = ares<256, ares<256, ares_down<256, SUBNET>>>;
+
+  template<class SUBNET>
+  using alevel2 = ares<128, ares<128, ares_down<128, SUBNET>>>;
+
+  template<class SUBNET>
+  using alevel3 = ares<64, ares<64, ares<64, ares_down<64, SUBNET>>>>;
+
+  template<class SUBNET>
+  using alevel4 = ares<32, ares<32, ares<32, SUBNET>>>;
+
+  using face_recognizer_type = dlib::loss_metric<dlib::fc_no_bias<128, dlib::avg_pool_everything<alevel0<alevel1<alevel2
+      <alevel3<alevel4<dlib::max_pool<3, 3, 2, 2, dlib::relu<dlib::affine<dlib::con<32, 7, 7, 2, 2,
+      dlib::input_rgb_image_sized<150>>>>>>>>>>>>>;
+
+  /** The type of encoded faces. */
+  using face_encoding_type = dlib::matrix<double, 0, 1>;
+
+  /** The map associating encoded faces with their IDs. */
+  std::map<int, face_encoding_type> m_store;
+
+  /** The face detector model. */
   dlib::frontal_face_detector m_face_detector;
 
-  /** The face pose predictor. */
+  /** The face pose predictor model. */
   dlib::shape_predictor m_face_pose_predictor;
+
+  /** The face recognizer model. */
+  face_recognizer_type m_face_recognizer;
 
 public:
   Registry();
@@ -65,6 +144,12 @@ public:
 
   ~Registry() = default;
 
+private:
+  face_encoding_type encode_face_pose(dlib::array2d<dlib::rgb_pixel> image, dlib::full_object_detection pose);
+
+  face_encoding_type encode_face(const Image& image, int l, int t, int r, int b);
+
+public:
   /** Register a face image with a face ID. */
   void add_face(int fid, Image image);
 
@@ -74,18 +159,51 @@ public:
 
 Registry::Registry()
     : m_store()
-    , m_face_detector() {
+    , m_face_detector()
+    , m_face_pose_predictor()
+    , m_face_recognizer() {
   // Load dlib standard frontal face detector
   m_face_detector = dlib::get_frontal_face_detector();
 
   // Load 68-point face pose predictor
   // This dataset generalizes a bunch of known facial feature points
   dlib::deserialize("data/shape_predictor_68_face_landmarks.dat") >> m_face_pose_predictor;
+
+  // Load dlib face recognition network v1
+  // This model extracts personally-identifying facial information
+  // It takes as input pose prediction data from the model right above
+  dlib::deserialize("data/dlib_face_recognition_resnet_model_v1.dat") >> m_face_recognizer;
+}
+
+Registry::face_encoding_type Registry::encode_face_pose(dlib::array2d<dlib::rgb_pixel> image,
+    dlib::full_object_detection pose) {
+  // The following is distilled from:
+  // https://github.com/davisking/dlib/blob/master/tools/python/src/face_recognition.cpp
+  // The 0.25 value is the chip padding
+  std::vector<dlib::chip_details> dets(1, dlib::get_face_chip_details(pose, 150, 0.25));
+  dlib::array<dlib::matrix<dlib::rgb_pixel>> face_chips;
+  dlib::extract_image_chips(image, dets, face_chips);
+
+  // Get the personally-identifying face encoding
+  return dlib::matrix_cast<double>(m_face_recognizer(face_chips, 16)[0]);
+}
+
+Registry::face_encoding_type Registry::encode_face(const Image& image, int l, int t, int r, int b) {
+  // Convert image to dlib array
+  auto image_dlib = image_to_dlib_array(image);
+
+  // Upscale image to improve face recognition accuracy
+  dlib::pyramid_up(image_dlib);
+
+  // Predict pose of the face
+  auto pose = m_face_pose_predictor(image_dlib, dlib::rectangle(l, t, r, b));
+
+  // Encode the face from its pose
+  return encode_face_pose(std::move(image_dlib), pose);
 }
 
 void Registry::add_face(int fid, Image image) {
-  // TODO: Also precompute stats on the face
-  m_store[fid] = image;
+  m_store[fid] = encode_face(image, 0, 0, image.width, image.height);
 }
 
 void Registry::remove_face(int fid) {
@@ -209,31 +327,10 @@ public:
 static dlib::image_window window;
 
 void Recognizer::process_frame(const Image& frame) {
-  // Create a dlib image with same size
-  // The height == # rows and the width == # cols
-  dlib::array2d<dlib::rgb_pixel> frame_dlib(frame.height, frame.width);
+  // Convert frame to dlib array
+  auto frame_dlib = image_to_dlib_array(frame);
 
-  // Quickly scan through the frame to convert its bytes
-  // Python Image Library (PIL) uses a different format than dlib C++ library
-  for (int row = 0; row < frame.height; ++row) {
-    for (int col = 0; col < frame.width; ++col) {
-      // Compute pixel offset into the frame
-      // This is the byte offset (into the PIL image) of the first pixel component
-      auto pixel_offset = 3ul * (row * frame.width + col);
-
-      // Extract color components for this pixel
-      // This looks into the pixel a bit further for color data
-      auto r = (unsigned char) frame.bytes[pixel_offset + 0];
-      auto g = (unsigned char) frame.bytes[pixel_offset + 1];
-      auto b = (unsigned char) frame.bytes[pixel_offset + 2];
-
-      // Store away the pixel in the dlib frame
-      // This adopts dlib C++ format (with a custom pixel class)
-      frame_dlib[row][col] = dlib::rgb_pixel(r, g, b);
-    }
-  }
-
-  // Upscale image to improve face recognition accuracy
+  // Upscale frame to improve face recognition accuracy
   dlib::pyramid_up(frame_dlib);
 
   // Blur the image a bit to remove transient artifacts
@@ -242,8 +339,6 @@ void Recognizer::process_frame(const Image& frame) {
   dlib::gaussian_blur(frame_dlib, frame_dlib_blurred, 1);
 
   // FIXME: Remove this eventually
-  window.clear_overlay();
-  window.set_image(frame_dlib_blurred);
   std::vector<dlib::full_object_detection> shapes;
 
   // Find all faces in the frame
@@ -251,9 +346,37 @@ void Recognizer::process_frame(const Image& frame) {
     // Predict the pose of the face
     auto pose = registry->m_face_pose_predictor(frame_dlib_blurred, face);
 
+    // FIXME: Remove this eventually
     shapes.push_back(pose);
+
+    // Encode the face pose
+    auto encoding = registry->encode_face_pose(std::move(frame_dlib_blurred), pose); // FIXME: const ref rather than move?
+
+    // Go over all known encodings
+    // TODO: Maybe we can speed this up with some sort of hashing?
+    for (auto [k, v] : registry->m_store) {
+      // Compute difference between this and that encoding
+      auto diff = encoding - v;
+
+      // Compute dot product of difference vector with itself
+      // This is just Euclidean distance without the square root
+      // NOTE: diff is a column vector, so its data is vertical
+      double dot = 0;
+      for (int row = 0; row < diff.nr(); ++row) {
+        dot += diff(row, 0) * diff(row, 0);
+      }
+
+      // Check against tolerance
+      // TODO: Make this configurable
+      if (dot <= 0.64 * 0.64) {
+        std::cout << "MATCH with " << k << "\n";
+      }
+    }
   }
 
+  // FIXME: Remove this eventually
+  window.clear_overlay();
+  window.set_image(frame_dlib);
   window.add_overlay(dlib::render_face_detections(shapes));
 }
 
@@ -266,7 +389,7 @@ void Recognizer::processor_loop() {
       // Acquire pending frame lock
       // This will unlock automatically when it goes out of scope
       // It can also be transferred somewhere else (like a condition variable)
-      std::unique_lock<std::mutex> lock(m_pend_frame_mutex);
+      std::unique_lock lock(m_pend_frame_mutex);
 
       // Wait while frame is not present
       // Note that the CV steals the lock and periodically toggles it
@@ -317,26 +440,26 @@ void Recognizer::poll() {
 
   {
     // Lock and dispatch face show events
-    std::lock_guard<std::mutex> lock(m_pend_face_show_mutex);
+    std::lock_guard lock(m_pend_face_show_mutex);
     dispatch(m_pend_face_show, m_cbs_face_show);
   }
 
   {
     // Lock and dispatch face hide events
-    std::lock_guard<std::mutex> lock(m_pend_face_hide_mutex);
+    std::lock_guard lock(m_pend_face_hide_mutex);
     dispatch(m_pend_face_hide, m_cbs_face_hide);
   }
 
   {
     // Lock and dispatch face move events
-    std::lock_guard<std::mutex> lock(m_pend_face_move_mutex);
+    std::lock_guard lock(m_pend_face_move_mutex);
     dispatch(m_pend_face_move, m_cbs_face_move);
   }
 }
 
 void Recognizer::submit_frame(Image frame) {
   // Acquire pending frame lock
-  std::lock_guard<std::mutex> lock(m_pend_frame_mutex);
+  std::lock_guard lock(m_pend_frame_mutex);
 
   // Replace the pending frame
   m_pend_frame = std::move(frame);
